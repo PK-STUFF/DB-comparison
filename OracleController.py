7ff013e2b4d9cf76ec253141dfcc9649d25d363c
tzhvanko <tetianazhvanko@gmail.com>
2021-12-15 21:18:39 +0100

    fix

--- OracleController.py
+++ OracleController.py
@@ -19,23 +19,18 @@ class OracleController():
 
 
 	def create_author(self, identifier, data):
-		addr = self._create_address("author", identifier)
 		# Returns True if record was created, otherwise False
 		return True
 
 	def create_book(self, identifier, data):
-		addr = self._create_address("book", identifier)
-		data["id_aut"] = "REF::author:{}".format(data["id_aut"])
 		# Returns True if record was created, otherwise False
 		return True
 
 	def read_author(self, identifier):
-		addr = self._create_address("author", identifier)
 		# Returns data read from database as parsed JSON
 		return True
 
 	def read_book(self, identifier):
-		addr = self._create_address("book", identifier)
 		# Returns data read from database as parsed JSON
 		return True
 
@@ -48,11 +43,9 @@ class OracleController():
 		return True
 
 	def delete_author(self, identifier):
-		addr = self._create_address("author", identifier)
 		# Returns True if record was deleted, otherwise False
 		return True
 
 	def delete_book(self, identifier):
-		addr = self._create_address("book", identifier)
 		# Returns True if record was deleted, otherwise False
 		return True
