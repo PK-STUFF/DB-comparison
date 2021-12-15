80279486e1f6bd740d6540d0d235475d78f55550
tzhvanko <tetianazhvanko@gmail.com>
2021-12-15 20:55:32 +0100

    prepare methods template

--- OracleController.py
+++ OracleController.py
@@ -5,18 +5,43 @@ class OracleController():
 		# Init connection, etc.
 		raise NotImplementedError
 
-	def create(self, table, identifier, data):
-		# INSERT
-		raise NotImplementedError
 
-	def read(self, table, identifier):
-		# SELECT
-		raise NotImplementedError
 
-	def update(self, table, identifier, data):
-		# UPDATE
-		raise NotImplementedError
+	def create_author(self, identifier, data):
+		addr = self._create_address("author", identifier)
+		# Returns True if record was created, otherwise False
+		return True
 
-	def delete(self, table, identifier):
-		# DELETE
-		raise NotImplementedError
+	def create_book(self, identifier, data):
+		addr = self._create_address("book", identifier)
+		data["id_aut"] = "REF::author:{}".format(data["id_aut"])
+		# Returns True if record was created, otherwise False
+		return True
+
+	def read_author(self, identifier):
+		addr = self._create_address("author", identifier)
+		# Returns data read from database as parsed JSON
+		return True
+
+	def read_book(self, identifier):
+		addr = self._create_address("book", identifier)
+		# Returns data read from database as parsed JSON
+		return True
+
+	def update_author(self, identifier, data):
+		# Returns True if record was updated, otherwise False
+		return True
+
+	def update_book(self, identifier, data):
+		# Returns True if record was updated, otherwise False
+		return True
+
+	def delete_author(self, identifier):
+		addr = self._create_address("author", identifier)
+		# Returns True if record was deleted, otherwise False
+		return True
+
+	def delete_book(self, identifier):
+		addr = self._create_address("book", identifier)
+		# Returns True if record was deleted, otherwise False
+		return True
