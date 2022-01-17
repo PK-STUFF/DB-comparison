5a66912c0d081c9c396e4efecc38bb921451cc6f
tzhvanko <tetianazhvanko@gmail.com>
2022-01-17 22:17:49 +0100

    remove connection from methods in Oracle Controller

--- OracleController.py
+++ OracleController.py
@@ -2,12 +2,14 @@ import cx_Oracle
 
 class OracleController():
 	name = "OracleDB"
+	# conn = None
 
 	def __init__(self, addr, ic_path):
 		# Init connection, etc.
 		cx_Oracle.init_oracle_client(lib_dir=ic_path)
 		self.addr = addr
 		self.ic_path = ic_path
+		self.conn = self.connect()
 
 	def connect(self):
 		conn = cx_Oracle.connect(user="SYS", 
@@ -21,12 +23,12 @@ class OracleController():
 		# Returns True if record was created, otherwise False
 		cmd=f"insert into author(id_aut, fname, lname, country) values( {identifier}, '{data['fname']}', '{data['lname']}', '{data['country']}') returning id_aut into :id_aut"
 		
-		conn = self.connect()
-		c = conn.cursor()
+		# conn = self.connect()
+		c = self.conn.cursor()
 		id_aut_wrap = c.var(cx_Oracle.NUMBER)
 		sql_params = { "id_aut" : id_aut_wrap }
 		c.execute(cmd, sql_params)
-		conn.commit()
+		self.conn.commit()
 
 		id_aut=id_aut_wrap.getvalue()
 		if id_aut:
@@ -38,12 +40,12 @@ class OracleController():
 		# Returns True if record was created, otherwise False
 		cmd=f"insert into book(id_book, title, price, id_aut, pages, date_pub) values( {identifier}, '{data['title']}', {data['price']}, {data['id_aut']}, {data['pages']}, to_date('{data['date_pub']}', 'YYYY-MM-DD HH24:MI:SS')) returning id_book into :id_book"
 		
-		conn = self.connect()
-		c = conn.cursor()
+		# conn = self.connect()
+		c = self.conn.cursor()
 		id_book_wrap = c.var(cx_Oracle.NUMBER)
 		sql_params = { "id_book" : id_book_wrap }
 		c.execute(cmd, sql_params)
-		conn.commit()
+		self.conn.commit()
 
 		id_book=id_book_wrap.getvalue()
 		if id_book:
@@ -54,9 +56,9 @@ class OracleController():
 	def read_author(self, identifier):
 		cmd = f"select * from author where id_aut ={identifier}"
 
-		conn = self.connect()
-		c = conn.cursor()
-		c = conn.cursor()
+		# conn = self.connect()
+		c = self.conn.cursor()
+		c = self.conn.cursor()
 		c.execute(cmd)
 
 		return True
@@ -64,9 +66,9 @@ class OracleController():
 	def read_book(self, identifier):
 		cmd = f"select * from author where id_aut ={identifier}"
 
-		conn = self.connect()
-		c = conn.cursor()
-		c = conn.cursor()
+		# conn = self.connect()
+		c = self.conn.cursor()
+		c = self.conn.cursor()
 		c.execute(cmd)
 				
 		return True
@@ -75,12 +77,12 @@ class OracleController():
 		# Returns True if record was updated, otherwise False
 		cmd=f"update author set fname='{data['fname']}', lname='{data['lname']}', country='{data['country']}' where id_aut={identifier} returning id_aut into :id_aut"
 		
-		conn = self.connect()
-		c = conn.cursor()
+		# conn = self.connect()
+		c = self.conn.cursor()
 		id_aut_wrap = c.var(cx_Oracle.NUMBER)
 		sql_params = { "id_aut" : id_aut_wrap }
 		c.execute(cmd, sql_params)
-		conn.commit()
+		self.conn.commit()
 
 		id_aut=id_aut_wrap.getvalue()
 		if id_aut:
@@ -92,12 +94,12 @@ class OracleController():
 		# Returns True if record was updated, otherwise False
 		cmd=f"update book set title='{data['title']}', price={data['price']}, id_aut={data['id_aut']}, pages={data['pages']}, date_pub=to_date('{data['date_pub']}', 'YYYY-MM-DD HH24:MI:SS') where id_book={identifier} returning id_book into :id_book"
 		
-		conn = self.connect()
-		c = conn.cursor()
+		# conn = self.connect()
+		c = self.conn.cursor()
 		id_book_wrap = c.var(cx_Oracle.NUMBER)
 		sql_params = { "id_book" : id_book_wrap }
 		c.execute(cmd, sql_params)
-		conn.commit()
+		self.conn.commit()
 
 		id_book=id_book_wrap.getvalue()
 		if id_book:
@@ -109,12 +111,12 @@ class OracleController():
 		# Returns True if record was deleted, otherwise False
 		cmd=f"delete from author where id_aut={identifier} returning id_aut into :id_aut"
 		
-		conn = self.connect()
-		c = conn.cursor()
+		# conn = self.connect()
+		c = self.conn.cursor()
 		id_aut_wrap = c.var(cx_Oracle.NUMBER)
 		sql_params = { "id_aut" : id_aut_wrap }
 		c.execute(cmd, sql_params)
-		conn.commit()
+		self.conn.commit()
 
 		id_aut=id_aut_wrap.getvalue()
 		if id_aut:
@@ -126,12 +128,12 @@ class OracleController():
 		# Returns True if record was deleted, otherwise False
 		cmd=f"delete from book where id_book={identifier} returning id_book into :id_book"
 		
-		conn = self.connect()
-		c = conn.cursor()
+		# conn = self.connect()
+		c = self.conn.cursor()
 		id_book_wrap = c.var(cx_Oracle.NUMBER)
 		sql_params = { "id_book" : id_book_wrap }
 		c.execute(cmd, sql_params)
-		conn.commit()
+		self.conn.commit()
 
 		id_book=id_book_wrap.getvalue()
 		if id_book:
