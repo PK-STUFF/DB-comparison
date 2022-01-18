08a341e4ec0e2572c2ece6e206fed667bae4ef23
tzhvanko <tetianazhvanko@gmail.com>
2022-01-18 13:00:18 +0100

    remove duplicates

--- OracleController.py
+++ OracleController.py
@@ -2,7 +2,6 @@ import cx_Oracle
 
 class OracleController():
 	name = "OracleDB"
-	# conn = None
 
 	def __init__(self, addr, ic_path):
 		# Init connection, etc.
@@ -23,15 +22,13 @@ class OracleController():
 		# Returns True if record was created, otherwise False
 		cmd=f"insert into author(id_aut, fname, lname, country) values( {identifier}, '{data['fname']}', '{data['lname']}', '{data['country']}') returning id_aut into :id_aut"
 		
-		# conn = self.connect()
 		c = self.conn.cursor()
 		id_aut_wrap = c.var(cx_Oracle.NUMBER)
 		sql_params = { "id_aut" : id_aut_wrap }
 		c.execute(cmd, sql_params)
 		self.conn.commit()
 
-		id_aut=id_aut_wrap.getvalue()
-		if id_aut:
+		if id_aut_wrap.getvalue():
 			return True
 		else:
 			return False
@@ -40,34 +37,28 @@ class OracleController():
 		# Returns True if record was created, otherwise False
 		cmd=f"insert into book(id_book, title, price, id_aut, pages, date_pub) values( {identifier}, '{data['title']}', {data['price']}, {data['id_aut']}, {data['pages']}, to_date('{data['date_pub']}', 'YYYY-MM-DD HH24:MI:SS')) returning id_book into :id_book"
 		
-		# conn = self.connect()
 		c = self.conn.cursor()
 		id_book_wrap = c.var(cx_Oracle.NUMBER)
 		sql_params = { "id_book" : id_book_wrap }
 		c.execute(cmd, sql_params)
 		self.conn.commit()
 
-		id_book=id_book_wrap.getvalue()
-		if id_book:
+		if id_book_wrap.getvalue():
 			return True
 		else:
 			return False
 
 	def read_author(self, identifier):
 		cmd = f"select * from author where id_aut ={identifier}"
-
-		# conn = self.connect()
-		c = self.conn.cursor()
+	
 		c = self.conn.cursor()
 		c.execute(cmd)
 
 		return True
 
 	def read_book(self, identifier):
-		cmd = f"select * from author where id_aut ={identifier}"
+		cmd = f"select * from book where id_book ={identifier}"
 
-		# conn = self.connect()
-		c = self.conn.cursor()
 		c = self.conn.cursor()
 		c.execute(cmd)
 				
@@ -77,15 +68,13 @@ class OracleController():
 		# Returns True if record was updated, otherwise False
 		cmd=f"update author set fname='{data['fname']}', lname='{data['lname']}', country='{data['country']}' where id_aut={identifier} returning id_aut into :id_aut"
 		
-		# conn = self.connect()
 		c = self.conn.cursor()
 		id_aut_wrap = c.var(cx_Oracle.NUMBER)
 		sql_params = { "id_aut" : id_aut_wrap }
 		c.execute(cmd, sql_params)
 		self.conn.commit()
 
-		id_aut=id_aut_wrap.getvalue()
-		if id_aut:
+		if id_aut_wrap.getvalue():
 			return True
 		else:
 			return False
@@ -94,15 +83,13 @@ class OracleController():
 		# Returns True if record was updated, otherwise False
 		cmd=f"update book set title='{data['title']}', price={data['price']}, id_aut={data['id_aut']}, pages={data['pages']}, date_pub=to_date('{data['date_pub']}', 'YYYY-MM-DD HH24:MI:SS') where id_book={identifier} returning id_book into :id_book"
 		
-		# conn = self.connect()
 		c = self.conn.cursor()
 		id_book_wrap = c.var(cx_Oracle.NUMBER)
 		sql_params = { "id_book" : id_book_wrap }
 		c.execute(cmd, sql_params)
 		self.conn.commit()
 
-		id_book=id_book_wrap.getvalue()
-		if id_book:
+		if id_book_wrap.getvalue():
 			return True
 		else:
 			return False
@@ -111,15 +98,13 @@ class OracleController():
 		# Returns True if record was deleted, otherwise False
 		cmd=f"delete from author where id_aut={identifier} returning id_aut into :id_aut"
 		
-		# conn = self.connect()
 		c = self.conn.cursor()
 		id_aut_wrap = c.var(cx_Oracle.NUMBER)
 		sql_params = { "id_aut" : id_aut_wrap }
 		c.execute(cmd, sql_params)
 		self.conn.commit()
-
-		id_aut=id_aut_wrap.getvalue()
-		if id_aut:
+	
+		if id_aut_wrap.getvalue():
 			return True
 		else:
 			return False
@@ -128,16 +113,13 @@ class OracleController():
 		# Returns True if record was deleted, otherwise False
 		cmd=f"delete from book where id_book={identifier} returning id_book into :id_book"
 		
-		# conn = self.connect()
 		c = self.conn.cursor()
 		id_book_wrap = c.var(cx_Oracle.NUMBER)
 		sql_params = { "id_book" : id_book_wrap }
 		c.execute(cmd, sql_params)
 		self.conn.commit()
 
-		id_book=id_book_wrap.getvalue()
-		if id_book:
+		if id_book_wrap.getvalue():
 			return True
 		else:
 			return False
-
